b45a4fd04f334d64ff4bb085a4ee2a0a6549508b
hayabusaSS <sanachanpromin@gmail.com>
2020-09-13 08:48:02 +0000

    add column

---
+++ db/migrate/20200913081213_ad_dstatus_totasks.rb
@@ -0,0 +1,4 @@
+class AdDstatusTotasks < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_011328) do
+ActiveRecord::Schema.define(version: 2020_09_13_081213) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
